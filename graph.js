47a9689c2f0bb78cfbf5ba17c2d28689484d1da7
ShailynOrtiz <sortiz@intellisys.com.do>
2018-04-05 23:29:48 -0400

    group age graphic

--- graph.js
+++ graph.js
@@ -1,52 +1,95 @@
-function draw(data) {
+function makeSvg(width, height, margin) {
 
-    /*
-    D3.js setup code
-    */
-
-    "use strict";
-    var margin = 75,
-    width = 1400 - margin,
-    height = 600 - margin;
-
-    d3.select("body")
-        .append("h2")
-        .text("Titanic survivor rate")
-
-    var svg = d3.select("body")
+    return d3.select("body")
         .append("svg")
         .attr("width", width + margin)
         .attr("height", height + margin)
         .append('g')
         .attr('class','chart');
 
-    /*
-    Dimple.js Chart construction code
-    */
+};
 
-    // var myChart = new dimple.chart(svg, data);
-    // var x = myChart.addCategoryAxis('x', 'Age');
-    // x.addOrderRule('Age')
-    // myChart.addMeasureAxis('y','Survived')
-    // myChart.addSeries("Pclass", dimple.plot.scatter);
-    // myChart.draw();
+function makeAgeGroup(data) {
 
-    // var myChart = new dimple.chart(svg, data);
-    // var y = myChart.addMeasureAxis('y', 'Survived');
+    var transformed = []
 
-    // var x = myChart.addCategoryAxis('x', 'Age');
-    // x.addOrderRule('Age');
-    // // myChart.addSeries("Pclass", dimple.plot.scatter);
-    // var series = myChart.addSeries("Sex", dimple.plot.bar);
+    data.forEach(
+        function(datapoint, index){
 
-    var myChart = new dimple.chart(svg, data);
+            var Age = datapoint.Age
 
-    myChart.setBounds(60, 30, 510, 305)
+            if (Age != 0){
 
-    var x = myChart.addCategoryAxis("x", "Age");
-    x.addOrderRule("Age");
-    myChart.addMeasureAxis("y", "Survived");
-    myChart.addSeries("Sex", dimple.plot.bar);
-    myChart.addLegend(60, 10, 510, 20, "right");
+                if (Age > 0 && Age <= 10){
+                    ageGroup = '1-10';
+                } else if (Age > 10 && Age <= 20){
+                    ageGroup = '11-20';
+                } else if (Age > 20 && Age <= 30){
+                    ageGroup = '21-30';
+                } else if (Age > 30 && Age <= 40){
+                    ageGroup = '31-40';
+                } else if (Age > 40 && Age <= 50){
+                    ageGroup = '41-50';
+                } else if (Age > 50 && Age <= 60){
+                    ageGroup = '51-60';
+                } else if (Age > 60 && Age <= 70){
+                    ageGroup = '61-70';
+                } else if (Age > 70 && Age <= 80){
+                    ageGroup = '71-80';
+                }
+
+                datapoint.Age = ageGroup
+                transformed.push(datapoint);
+            }
+        }
+    )
+
+    return transformed
+};
+
+function addTitle(title, tag='h2', inside='body') {
+   return d3.select(inside).append(tag).text(title)
+}
+
+function draw(data) {
+
+    "use strict";
+    var margin = 75,
+    width = 1400 - margin,
+    height = 600 - margin;
+
+    var pageTitle = addTitle(
+        "Titanic Survivers by grop's Age"
+    )
+
+    var data = makeAgeGroup(data)
+
+    // Bar chart
+    var firstGraph = makeSvg(width, height, margin);
+    var myChart = new dimple.chart(
+        firstGraph,
+        data,
+    );
+    var x = myChart.addCategoryAxis('x', 'Age');
+    x.title = "Age group";
+    x.addOrderRule('Age')
+    var y = myChart.addMeasureAxis('y','Survived')
+    y.title = 'Survivers'
+    myChart.addSeries(null, dimple.plot.bar);
+    myChart.draw();
+
+    // Scatter-plot + line chart
+    var secondGraph = makeSvg(width, height, margin);
+    var myChart = new dimple.chart(
+        secondGraph,
+        data,
+    );
+    var x = myChart.addCategoryAxis('x', 'Age');
+    x.title = "Age group";
+    x.addOrderRule('Age')
+    var y = myChart.addMeasureAxis('y','Survived')
+    y.title = 'Survivers'
+    myChart.addSeries(null, dimple.plot.line);
+    myChart.addSeries(null, dimple.plot.scatter);
     myChart.draw();
 };
